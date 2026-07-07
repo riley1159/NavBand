47a500ff42fc0ee84b79ac1bdc11754345845d1e
Riley <rileyap0615@gmail.com>
2026-07-07 08:50:13 -0400

    Update NavBandUITests.swift

--- NavBandUITests/NavBandUITests.swift
+++ NavBandUITests/NavBandUITests.swift
@@ -1,21 +1,12 @@
-//
-//  NavBandUITests.swift
-//  NavBandUITests
-//
-//  Created by Jason Brock on 2025-03-30.
-//
-
 import XCTest
 
 final class NavBandUITests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+        // Put setup code here. 
+        
         continueAfterFailure = false
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
     override func tearDownWithError() throws {
